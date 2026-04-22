becfe6d977deb990962cb54f169206be4587cf95
ATHANASIOS ANGELIS <121366337+mrphoenix300@users.noreply.github.com>
2026-04-22 22:07:27 +0300

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        frequency_dict = Counter(nums)
+        # max_frequency = max(frequency_dict.values())
+        max_key = max(frequency_dict, key=frequency_dict.get)
+
+        return max_key
