e3ade92f2ad2d871c9cde2b58fd34398bba45ed0
ATHANASIOS ANGELIS <121366337+mrphoenix300@users.noreply.github.com>
2026-05-01 18:13:51 +0300

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        index_map = defaultdict(list)
+        for i in range(len(nums)):
+            index_map[nums[i]].append(i)
+
+        for num in nums:
+            if len(index_map[num]) == 1:
+                index_map.pop(num)
+
+        for indices in index_map.values():
+            i = 0
+            for j in range(i + 1, len(indices)):
+                if abs(indices[i] - indices[j]) <= k:
+                    return True
+                i += 1
+        return False
+
+
+            
+
+        
+        return False
+        
